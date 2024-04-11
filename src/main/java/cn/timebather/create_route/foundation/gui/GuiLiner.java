051934a3cc380fbc2a3ff8defaf711e7ba32d4d2
TimeBather <open-source@timebather.cn>
2024-04-11 23:54:48 +0800

    feat(gui): implement GuiLiner

---
+++ src/main/java/cn/timebather/create_route/foundation/gui/GuiLiner.java
@@ -0,0 +1,43 @@
+package cn.timebather.create_route.foundation.gui;
+
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.util.Mth;
+import org.joml.Matrix4f;
+
+import java.util.Collection;
+import java.util.function.BiConsumer;
+
+public class GuiLiner {
+    public static BiConsumer<VertexConsumer,Matrix4f> line(float x1, float y1, float x2, float y2, float width, Color color){
+        float k = - (x1 - x2) / (y1 - y2); // k1 = -1/k
+        float offsetX = width * (float) Mth.fastInvSqrt(1f+Math.abs(k)); // sqrt(1+k1^2) * oX = width , oX = width / sqrt(1+k1^2)
+        float offsetY = k * offsetX;
+        float vertexX1 = x1 - offsetX,
+              vertexX2 = x1 + offsetX,
+              vertexX3 = x2 - offsetX,
+              vertexX4 = x2 + offsetX;
+        float vertexY1 = y1 - offsetY,
+              vertexY2 = y1 + offsetY,
+              vertexY3 = y2 - offsetY,
+              vertexY4 = y2 + offsetY;
+        float colorR = color.getRedPercentage(),
+                colorG = color.getGreenPercentage(),
+                colorB = color.getBluePercentage(),
+                colorA = color.getAlphaPercentage();
+        return (consumer,matrix4f)->{
+            consumer.vertex(matrix4f,vertexX1,vertexY1,0).color(colorR,colorG,colorB,colorA).endVertex();
+            consumer.vertex(matrix4f,vertexX3,vertexY3,0).color(colorR,colorG,colorB,colorA).endVertex();
+            consumer.vertex(matrix4f,vertexX4,vertexY4,0).color(colorR,colorG,colorB,colorA).endVertex();
+            consumer.vertex(matrix4f,vertexX2,vertexY2,0).color(colorR,colorG,colorB,colorA).endVertex();
+        };
+    }
+
+    public static void batchDraw(MultiBufferSource bufferSource, Matrix4f pose, Collection<BiConsumer<VertexConsumer,Matrix4f>> lines){
+        VertexConsumer consumer = bufferSource.getBuffer(RenderType.gui());
+        for (BiConsumer<VertexConsumer, Matrix4f> line : lines) {
+            line.accept(consumer,pose);
+        }
+    }
+}

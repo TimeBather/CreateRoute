3c15f7eaebb2b5123316d93a9b1c2e913251e629
TimeBather <open-source@timebather.cn>
2024-04-14 14:10:04 +0800

    remove: remove default panel device

--- src/main/java/cn/timebather/create_route/content/train/devices/panel/default_panel/DefaultPanelDevice.java
+++
@@ -1,4 +0,0 @@
-package cn.timebather.create_route.content.train.devices.panel.default_panel;
-
-public class DefaultPanelDevice {
-}
--- src/main/java/cn/timebather/create_route/content/train/devices/panel/default_panel/item/DefaultPanelItem.java
+++
@@ -1,4 +0,0 @@
-package cn.timebather.create_route.content.train.devices.panel.default_panel.item;
-
-public class DefaultPanelItem {
-}

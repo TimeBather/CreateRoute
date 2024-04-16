a678ba6008b17f21d70e7e430518a5e81fe55294
TimeBather <open-source@timebather.cn>
2024-04-16 21:08:34 +0800

    refactor: split background renderers of LKJ2000 into LKJ2000BackgroundRenderer

--- src/main/java/cn/timebather/create_route/content/train/devices/atp/lkj2000/screen/LKJ2000Screen.java
+++ src/main/java/cn/timebather/create_route/content/train/devices/atp/lkj2000/screen/LKJ2000Screen.java
@@ -88,19 +88,8 @@ public class LKJ2000Screen extends Screen {
         poseStack.translate(width,height - 20,0);
         poseStack.scale(0.75f,0.75f,1f);
         poseStack.translate( - 256,- 192 - 20,0);
-        LKJ2000GuiComponents.LKJ_ENTRY.render(graphics,0,0);
-        LKJ2000GuiComponents.LKJ_SIGNAL_AREA.render(graphics,9,7);
-        LKJ2000GuiComponents.LKJ_SIGNAL.render(graphics, Color.WHITE,9+2,7+2);
-        LKJ2000GuiComponents.LKJ_SIGNAL_LIGHT.render(graphics,9+2+1,7+2+1);
-        LKJ2000GuiComponents.LKJ_FONT_SPEED.withScale(0.35).render(graphics,Color.BLUE ,45,10);
-        LKJ2000GuiComponents.LKJ_FONT_SPEED_LIMIT.withScale(0.35).render(graphics,Color.BLUE,81,10);
-        LKJ2000GuiComponents.LKJ_FONT_DISTANCE.withScale(0.35).render(graphics,Color.BLUE,116,10);
-        LKJ2000GuiComponents.LKJ_FONT_SIGNAL.render(graphics,Color.BLUE,170,8,10,6);
-        LKJ2000GuiComponents.LKJ_FONT_RAIL_DISTANCE.render(graphics,Color.BLUE,170,17,9,6);
 
-        LKJ2000GuiComponents.LKJ_FONT_SLOPE.render(graphics,Color.BLACK,8,145,15,8);
-        LKJ2000GuiComponents.LKJ_FONT_CURVE.render(graphics,Color.BLACK,8,156,15,8);
-        LKJ2000GuiComponents.LKJ_FONT_RBC.render(graphics,Color.BLACK,8,168,15,8);
+        LKJ2000BackgroundRenderer.render(graphics);
 
         renderStatus(graphics,Color.YELLOW,Color.BLACK,LKJ2000GuiComponents.LKJ_FONT_DOWNGRADE,33);
         renderStatus(graphics,Color.RED,Color.YELLOW,LKJ2000GuiComponents.LKJ_FONT_EMERGENCY_BREAK,43);
